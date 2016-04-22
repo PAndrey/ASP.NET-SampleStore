a53da29eac923c80e9a487bdf3d1862948b2dc3b
Petrov A.V. <petrov.a.v.mail@yandex.ru>
2016-04-22 15:13:30 +0400

    1

---
+++ SmartStore.Model/Entities/Concrete/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleStore.Model.Entities;
+using SampleStore.Model.Entities.Abstract;
+
+namespace SampleStore.Model.Entities.Concrete
+{
+  public class ProductRepository : IProductRepository
+  {
+    EFDbContext context = new EFDbContext();
+    public IQueryable<Product> Products
+    {
+  	  get 
+      {
+        return context.Products;
+      }
+
+      set 
+      {
+//        context.Products = Products;
+        context.SaveChanges();
+      }
+    }
+  }
+}
---
+++ SmartStore.Model/Entities/EFDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SampleStore.Model.Entities;
+
+namespace SampleStore.Model.Entities
+{
+  public class EFDbContext : DbContext
+  {
+    // Доступ к продуктам
+    public DbSet<Product> Products { get; set; }
+    // Доступ к пользователям
+    public DbSet<User>    Users { get; set; }
+  }
+}
