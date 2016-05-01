dc747648fad3c7af93e2be9f4fccdb5f026f372e
Petrov A.V <petrov.a.v.mail@gmail.com>
2016-05-01 23:52:49 +0300

    Проба модели для передачи данных во View.
    Сделан класс модели отображения, который включает в себя два класса домена приложения.

--- SampleStore.WebUI/Controllers/HomeController.cs
+++ SampleStore.WebUI/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 using SampleStore.Domain.Entities.Abstract;
 using SampleStore.Domain.Entities;
+using SampleStore.Domain.Entities.Concrete;
+
+using SampleStore.WebUI.Models;
 
 namespace SampleStore.WebUI.Controllers
 {
@@ -16,14 +19,21 @@ namespace SampleStore.WebUI.Controllers
 
         public ActionResult Index()
         {
-          Product[] products = new Product[] 
-          {
-            new Product { ProductId = 0, Name = "USB Drive", Price = 600, Description = "USB-Флеш на 32 Гб."},
-            new Product { ProductId = 1, Name = "HDD Drive", Price = 1600, Description = "HDD 1-Тб."},
-            new Product { ProductId = 2, Name = "iMac", Price = 150000, Description = "Персональный ПК от Apple."},
-            new Product { ProductId = 3, Name = "iPhone", Price = 35000, Description = "Телефон от Apple."}
-          };
-          return View(products.AsEnumerable());
+          /*
+                    Product[] products = new Product[] 
+                    {
+                      new Product { ProductId = 0, Name = "USB Drive", Price = 600, Description = "USB-Флеш на 32 Гб."},
+                      new Product { ProductId = 1, Name = "HDD Drive", Price = 1600, Description = "HDD 1-Тб."},
+                      new Product { ProductId = 2, Name = "iMac", Price = 150000, Description = "Персональный ПК от Apple."},
+                      new Product { ProductId = 3, Name = "iPhone", Price = 35000, Description = "Телефон от Apple."}
+                    };
+
+                     return View(products.AsEnumerable());
+           */
+
+          IndexPageModel model = new IndexPageModel();
+
+          return View(model);
         }
 
     }
---
+++ SmartStore.Model/Entities/Concrete/UsersRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleStore.Domain.Entities.Abstract;
+using SampleStore.Domain.Entities;
+
+namespace SampleStore.Domain.Entities.Concrete
+{
+  public class UsersRepository : IUserRepository
+  {
+    EFDbContext context = new EFDbContext();
+    public IQueryable<User> Users
+    {
+      get
+      {
+        return context.Users;
+      }
+      set
+      {
+        context.SaveChanges();
+      }
+    }
+  }
+}
